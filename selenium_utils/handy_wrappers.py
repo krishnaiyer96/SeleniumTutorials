8c319cc77380971bc6b6c791652ab9a069c8373c
krishnaiyer96 <krishna.iyer17@gmail.com>
2024-12-05 08:36:41 +0530

    Pusing Wrapper class

--- selenium_utils/handy_wrappers.py
+++ selenium_utils/handy_wrappers.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 class HandyWrappers:
     def __init__(self, driver):
         self.driver = driver
